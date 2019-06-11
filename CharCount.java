9cafcc7c1fb5c5d9d5ce7e9f2db0575c3fafacad
praveencr7 <33199808+praveencr7@users.noreply.github.com>
2019-06-11 14:06:49 +0530

    Create CharCount.java

---
+++ CharCount.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CharCount {
+ public static void main(String[]args){
+	 Scanner sc = new Scanner(System.in);
+	 int count=0;
+	 String s = sc.nextLine();
+	 s=s.replace(" ", "");
+	 int a = s.length();
+	 System.out.println(a);
+	 }
+ }
